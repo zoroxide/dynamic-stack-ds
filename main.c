afdcf6c6aa3377d3bb255aafaaaf6d174540a23b
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2024-06-08 21:41:45 +0300

    init

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main() {
     StackStatus_t status;
-    int *data;
+
 
 
     // Create a stack with a maximum size of 5
@@ -15,29 +15,23 @@ int main() {
 
     // Push elements onto the stack
     for (int i = 0; i < 5; ++i) {
-        data = (int *)malloc(sizeof(int));
-        *data = i;
-        status = PushStack(myStack, data);
+        int data = i;
+        status = PushStack(myStack, &data);
         if (status != STACK_OK) {
             printf("Failed to push data onto stack\n");
             return -1;
         }
     }
 
-    // Try pushing one more element to check for stack overflow
-    data = (int *)malloc(sizeof(int));
-    *data = 5;
-    status = PushStack(myStack, data);
     if (status == STACK_FULL) {
         printf("Stack is full\n");
     }
 
     // Pop elements from the stack
     for (int i = 0; i < 5; ++i) {
-        data = (int *)PopStack(myStack, &status);
+        int *data = (int *)PopStack(myStack, &status);
         if (status == STACK_OK) {
             printf("Popped data: %d\n", *data);
-            free(data); // Free the memory allocated for the popped element
         } else {
             printf("Failed to pop data from stack\n");
         }
