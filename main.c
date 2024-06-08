92193d1224d5ee12929d345d6cd45998839b7d9e
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2024-06-08 18:44:53 +0300

    init

--- main.c
+++ main.c
@@ -7,23 +7,15 @@ StackStatue_t ret;
 Stack_t *mystack = 0;
 
 int main(void) {
-    // go to the header file and remove the DEBUG defenetion (at line 4) to disable debugging...
-    int desitred_val_to_push_1 = 3;
-    char desitred_val_to_push_2 = "test";
-    double desitred_val_to_push_3 = 3.14;
-    long long desitred_val_to_push_4 = 88;
-    int stackC;
-
 
     mystack = CreateStack(5, &ret);
 
+    // go to the header file and remove the DEBUG defenetion (at line 4) to disable debugging...
+    int desitred_val_to_push_1 = 3;PushStack(mystack, &desitred_val_to_push_1);
+    char desitred_val_to_push_2 = "test";PushStack(mystack, &desitred_val_to_push_2);
+    double desitred_val_to_push_3 = 3.14;PushStack(mystack, &desitred_val_to_push_3);
+    long long desitred_val_to_push_4 = 88;PushStack(mystack, &desitred_val_to_push_4);
 
-    PushStack(mystack, &desitred_val_to_push_4);
-    PushStack(mystack, &desitred_val_to_push_1);
-    PushStack(mystack, &desitred_val_to_push_3);
-    PushStack(mystack, &desitred_val_to_push_2);
-    PopStack(mystack, &ret);
-    StackCount(mystack, &stackC);
 
     DestroyStack(mystack, &ret);
     return 0;
